ed26e6153d4305082c6d791478b10a20de4310f3
Babajide Bello <abdulwalibello@gmail.com>
2025-12-10 11:25:48 +0100

    added newfile.c

--- Assignment11.c
+++ Assignment11.c
@@ -5,7 +5,7 @@
 int simpleHash(char password[]) {
     int hash = 0;
     for (int i = 0; password[i] != '\0'; i++) {
-        hash += password[i];  // sum of ASCII values
+        hash += password[i];
     }
     return hash;
 }
@@ -13,13 +13,9 @@ int simpleHash(char password[]) {
 int main() {
     FILE *file;
     char username[50], password[50];
-    int balance = 1000;
     char fileUsername[50];
     int fileHashedPassword;
     int balance = 1000;
-    int choice;
-    char input[50];
-    int amount;
 
     printf("Welcome to F3DS BANK\n");
     printf("SIGN UP\n");
@@ -27,27 +23,36 @@ int main() {
     scanf("%s", username);
     printf("Enter Password\n");
     scanf("%s", password);
-    
+
     int hashpassword = simpleHash(password);
 
-    file = fopen("user.txt", "W");
-    fprintf("%s\n%s\n%d", username, hashpassword, balance );
+    file = fopen("user.txt", "w");
+    fprintf(file, "%s\n%d\n%d", username, hashpassword, balance);
     fclose(file);
-    
-    printf("Account created successfully proceed to login\n");
-    while(1){
-    printf("Enter Username\n");
-    scanf("%s", username);
-    printf("Enter Password\n");
-    scanf("%s", password);
 
-    file = fopen("user.txt", "r");
-    fscanf(file, "%s", fileUsername);
-    fscanf(file, "%d", &fileHashedPassword);
+    printf("Account created successfully proceed to login\n");
+
+    while(1) {
+        printf("Enter Username\n");
+        scanf("%s", username);
+        printf("Enter Password\n");
+        scanf("%s", password);
+
+        file = fopen("user.txt", "r");
+        fscanf(file, "%s", fileUsername);
+        fscanf(file, "%d", &fileHashedPassword);
         fscanf(file, "%d", &balance);
         fclose(file);
-    
 
+        int hashedInput = simpleHash(password);
+
+        if(strcmp(username, fileUsername) == 0 && hashedInput == fileHashedPassword) {
+            printf("Login successful!\n");
+            break;
+        } else {
+            printf("Wrong username or password! Try again.\n");
+        }
     }
 
+    return 0;
 }
